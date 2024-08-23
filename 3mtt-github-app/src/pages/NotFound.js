092bdc9a4ae48af0878158ada329e141640c8790
A I Walter <idamsy4real@gmail.com>
2024-08-23 10:24:03 +0100

    restatrting the project

--- 3mtt-github-app/src/pages/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NotFound = () => (
-  <div className="p-6">
-    <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-  </div>
-);
-
-export default NotFound;
